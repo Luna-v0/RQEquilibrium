53b913ba670972131f0878b06bef865cc18ce81b
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-07-02 02:02:08 -0300

    preparing expirements

--- src/open_spiel_api.py
+++ src/open_spiel_api.py
@@ -4,33 +4,16 @@ import numpy as np
 import pyspiel
 
 
-class GameAPI:
-    def __init__(
-        self,
-        game_name: str,
-        decider: Callable[([int], [int]), int] = lambda x: np.random.choice(x),
-    ):
-        self.game = pyspiel.load_game(game_name)
-        self.state = self.game.new_initial_state()
-        self.decider = decider
+def get_matrix(game_name: str) -> list[np.ndarray]:
+    """
+    Load a matrix game from OpenSpiel and return its payoff matrix.
 
-    def reset(self):
-        """Reset the game to the initial state."""
-        self.state = self.game.new_initial_state()
+    Args:
+        game_name (str): The name of the game to load.
 
-    def step(self):
-        while not self.state.is_terminal():
-            legal_actions = self.state.legal_actions()
-            if self.state.is_chance_node():
-                outcomes_with_probs = self.state.chance_outcomes()
-                action_list, prob_list = zip(*outcomes_with_probs)
-                action = self.decider([action_list, prob_list])
-                if action not in action_list:
-                    raise ValueError(f"Action {action} is not a valid chance outcome.")
-                self.state.apply_action(action)
-            else:
-                if action in legal_actions:
-                    self.state.apply_action(action)
-                else:
-                    raise ValueError(f"Action {action} is not legal.")
-            return self.state.observation_tensor()
+    Returns:
+        np.ndarray: The payoff matrix of the game.
+    """
+    print(game_name)
+    game = pyspiel.load_matrix_game(game_name)
+    return [game.player_utilities(player) for player in range(game.num_players())]
